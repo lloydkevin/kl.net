95089c5c0b89ef7f56ffe1fb7a06f67723902b60
Kevin Lloyd <bazil749@gmail.com>
2009-01-07 00:01:18 -0600

    Moved "featured" to function.php

--- themes/kl/functions.php
+++ themes/kl/functions.php
@@ -1,4 +1,21 @@
 <?php
  if ( function_exists('register_sidebar') )
- register_sidebar();
+ 	register_sidebar();
+
+function featured() {
+	?>
+
+<div id="feature">
+	<div id="tag">
+		Web Developer, Web Designer, &amp; All Around Programmer. Started in '97 with Pascal; Migrated to C++, C#, PHP, JavaScript, Ruby on Rails, and more.
+	</div>
+	<ul>
+		<li>&gt; <a href="/portfolio">Check out my Portfolio</a></li>
+		<li>&gt; <a href="/wp-content/uploads/kevinlloyd_resume.pdf">Download My Resume</a></li>
+		<li>&gt; <a href="/hire-me"><strong>Hire Me!</strong></a></li>
+	</ul>
+</div>
+	
+<?php	
+}
 ?>
