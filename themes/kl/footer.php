daedc543645e76cbedefbd2857d3dbc728742d86
Kevin Lloyd <bazil749@gmail.com>
2009-01-21 22:44:35 -0600

    Update ver. number on files

--- themes/kl/footer.php
+++ themes/kl/footer.php
@@ -2,7 +2,7 @@
 			</div> <!-- page -->
 		</div> <!-- wrap -->
 		<div class="container">
-			<img src="<?php bloginfo('template_directory'); ?>/images/bottom_shadow.jpg" width="960" height="45" alt="Bottom Shadow" />
+			<img src="<?php bloginfo('template_directory'); ?>/images/bottom_shadow.jpg?v1" width="960" height="45" alt="Bottom Shadow" />
 		</div>
 		<div id="footer">
 			<?php wp_footer(); ?>
